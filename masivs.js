1cf190356370e31f6bd29949e796d82b09a5f9e0
kostt2 <53828504+kostt2@users.noreply.github.com>
2019-08-28 12:05:58 +0300

    aa

---
+++ masivs.js
@@ -0,0 +1,22 @@
+let masivs = [];
+
+function iegutPecIndeksa(i) {
+  return masivs[i];
+}
+
+function mainitPecIndeksa(i, vertiba) {
+  masivs[i] = vertiba;
+}
+
+function dzestPecIndeksa(i) {
+  masivs = masivs.splice(i, 1);
+}
+
+function atrastVertibasIndeksu(vertiba) {
+    for (let i = 0; i < masivs.length; i++) {
+      if ( vertiba === masivs[i]) {
+        return i;
+      }
+    }
+    return -1;
+  }
